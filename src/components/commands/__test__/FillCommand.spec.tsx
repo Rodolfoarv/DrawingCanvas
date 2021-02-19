ee8190dcf5bbf7be9db3f66975f5f48cb8d4c441
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2021-02-19 21:53:54 +0200

    ADDED Fill Command test

---
+++ src/components/commands/__test__/FillCommand.spec.tsx
@@ -0,0 +1,54 @@
+
+import {render, RenderResult} from "@testing-library/react";
+import React from "react";
+import {CanvasBoard} from "../../../screens/CanvasBoard";
+import {FillCommand} from "../FillCommand";
+import '@testing-library/jest-dom';
+
+let documentBody: RenderResult;
+describe("Rectangle", () => {
+
+    it('Should render a Invalid Command if canvas doesnt exist', () => {
+
+        documentBody = render(
+            <FillCommand command={["f","1", "1", "."]}/>
+        );
+        var linkElement = documentBody.getByLabelText("error");
+        expect(linkElement).toBeInTheDocument();
+    });
+
+
+    it('Should render a Invalid Command for wrong parameters', () => {
+        render(
+            <CanvasBoard command={"N 20 4"}/>
+        );
+
+        documentBody = render(
+            <FillCommand command={["f","1", "."]}/>
+        );
+        var linkElement = documentBody.getByText("ERROR: Invalid Command : ( Try: F x y color)");
+        expect(linkElement).toBeInTheDocument();
+    });
+
+
+
+
+    it('Should render a Rectangle', () => {
+
+        render(
+            <CanvasBoard command={"N 20 4"}/>
+        );
+
+        documentBody = render(
+            <FillCommand command={["f","1", "1", "."]}/>
+        );
+        var linkElement = documentBody.getAllByText(".");
+        expect(linkElement).toBeDefined();
+        expect(linkElement.length).toEqual(80);
+    });
+})
+
+
+
+
+

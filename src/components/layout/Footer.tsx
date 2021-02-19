2b6673e7b15089f87ed3c888880c7dd4cb0b7156
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2021-02-19 18:48:33 +0200

    ADDED Layout Structure

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = (): JSX.Element => {
+    return (
+        <footer className="footer mt-3">
+            <div className="content">
+                <p>
+                   by <a href="https://github.com/Rodolfoarv/Evenito">Rodolfo Andrés Ramírez Valenzuela</a>
+                </p>
+            </div>
+        </footer>
+    );
+}

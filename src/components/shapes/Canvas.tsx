9fab3172acd1f419010b6de4183573378a304d34
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2021-02-19 19:46:59 +0200

    ADDED Drawing canvas

---
+++ src/components/shapes/Canvas.tsx
@@ -0,0 +1,53 @@
+
+import React, {useEffect} from "react";
+import {CanvasProps} from "../../types/CanvasProps";
+import useCanvas from "../../hooks/useCanvas";
+import global from "../../store/global";
+
+export const Canvas = ({width, height, shapes, fill, removeShapes, removeFill}: CanvasProps) => {
+    const {border, canvasBody, drawShapes} = useCanvas(width, height);
+    const horizontalBorder = Array(width+2).fill(border.horizontal);
+
+    const drawEdge = (): any =>
+        horizontalBorder.map((value, i) => {
+            return <td key={"edge-"+i}>{value}</td>
+        });
+
+    useEffect(() => {
+        drawShapes(global.canvas.shapes, global.canvas.fillValues, removeShapes, removeFill);
+    }, [shapes, fill, canvasBody, drawShapes, removeShapes, removeFill])
+
+    return (
+        <div
+            style={{
+                textAlign: 'left',
+            }}>
+
+            {canvasBody &&
+            <table className="table is-narrow is-striped">
+                <thead>
+                <tr>{drawEdge()}</tr>
+                </thead>
+                <tbody>
+                {
+                    canvasBody.map((columns: any, i: number) => {
+                        var row = columns.map((value: any, j: number) => {
+                            return <td key={j}>{value}</td>
+                        });
+
+                        return <tr key={i}>
+                            <td>{border.vertical}</td>
+                            {row}
+                            <td>{border.vertical}</td>
+                        </tr>;
+
+                    })
+                }</tbody>
+                <tfoot>
+                <tr>{drawEdge()}</tr>
+                </tfoot>
+            </table>
+            }
+        </div>
+    );
+}

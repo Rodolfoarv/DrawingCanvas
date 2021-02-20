23bc7b7ef3893ea93b02c81cc1e3355ff82f580a
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2021-02-20 17:59:45 +0200

    Modified README and fixed mistake with fill test

--- src/components/commands/__test__/FillCommand.spec.tsx
+++ src/components/commands/__test__/FillCommand.spec.tsx
@@ -1,7 +1,7 @@
 
 import {render, RenderResult} from "@testing-library/react";
 import React from "react";
-import {DrawingBoard} from "../../../screens/DrawingBoard";
+import {CanvasBoard} from "../../../screens/CanvasBoard";
 import {FillCommand} from "../FillCommand";
 import '@testing-library/jest-dom';
 
@@ -20,13 +20,13 @@ describe("Rectangle", () => {
 
     it('Should render a Invalid Command for wrong parameters', () => {
         render(
-            <DrawingBoard command={"N 20 4"}/>
+            <CanvasBoard command={"N 20 4"}/>
         );
 
         documentBody = render(
-            <FillCommand command={["f","1", "."]}/>
+            <FillCommand command={["b","1"]}/>
         );
-        var linkElement = documentBody.getByText("ERROR: Invalid Command : ( Try: F x y color)");
+        var linkElement = documentBody.getByText("ERROR: Invalid Command : ( Try: B x y)");
         expect(linkElement).toBeInTheDocument();
     });
 })
